f743fd7adde2742a3a8df227845c394db3b714c8
Josh Deare <dearej@onid.orst.edu>
2014-07-07 14:30:46 -0700

    fixed cli in question 5

--- homework_2/question5.py
+++ homework_2/question5.py
@@ -7,6 +7,7 @@ import subprocess
 import shlex
 import os
 import sys
+import getopt
 
 def who_sp(options = None):
     # initialize base command
@@ -17,7 +18,6 @@ def who_sp(options = None):
 
     # split the command into CSV
     command_sequence = shlex.split(command)
-    
     # Start the subprocess
     who = subprocess.Popen(command_sequence, stdout=subprocess.PIPE)
     # who_out is now acting as the pipe receiver
@@ -29,9 +29,16 @@ def who_sp(options = None):
 
 def main(argv):
     # Accepting CLI args for extra credit
+    try:
+        opts, args = getopt.getopt(argv, "bdlprTtu")
+    except getopt.GetoptError:
+        print "Usage: question5.py (all optional args) -b -d -l -p -r -T -t -y"
+        sys.exit(2)
     arg_str = ""
-    for arg in argv:
-        arg_str = arg_str + arg
+    
+    # Construct the arg string
+    for opt, arg in opts:
+        arg_str = arg_str + opt + " "
     who_sp(arg_str)
 
 if __name__ == "__main__":
